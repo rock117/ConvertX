a152c2730325c670cde3d71ccf3b7048b8bb909e
rock huang <manj22@163.com>
2026-03-01 10:06:38 +0800

    feat: add drag-to-resize panels

---
+++ lib/src/widgets/resizable_divider.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class ResizableDivider extends StatefulWidget {
+  final Axis direction;
+  final double initialSize;
+  final double minSize;
+  final double maxSize;
+  final ValueChanged<double>? onResize;
+  final Color? color;
+  final Color? hoverColor;
+
+  const ResizableDivider({
+    super.key,
+    this.direction = Axis.horizontal,
+    this.initialSize = 0.7,
+    this.minSize = 0.2,
+    this.maxSize = 0.9,
+    this.onResize,
+    this.color,
+    this.hoverColor,
+  });
+
+  @override
+  State<ResizableDivider> createState() => _ResizableDividerState();
+}
+
+class _ResizableDividerState extends State<ResizableDivider> {
+  late double _size;
+  bool _isHovering = false;
+  bool _isDragging = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _size = widget.initialSize;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isHorizontal = widget.direction == Axis.horizontal;
+
+    return MouseRegion(
+      cursor: isHorizontal
+          ? SystemMouseCursors.resizeRow
+          : SystemMouseCursors.resizeColumn,
+      onEnter: (_) => setState(() => _isHovering = true),
+      onExit: (_) => setState(() => _isHovering = false),
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onPanStart: (_) => setState(() => _isDragging = true),
+        onPanEnd: (_) => setState(() => _isDragging = false),
+        onPanCancel: () => setState(() => _isDragging = false),
+        onPanUpdate: (details) {
+          final box = context.findRenderObject() as RenderBox?;
+          if (box == null) return;
+
+          final parentBox = box.parent as RenderBox?;
+          if (parentBox == null) return;
+
+          final parentSize = parentBox.size;
+          final localDelta = isHorizontal ? details.delta.dy : details.delta.dx;
+
+          setState(() {
+            if (isHorizontal) {
+              _size += localDelta / parentSize.height;
+            } else {
+              _size += localDelta / parentSize.width;
+            }
+            _size = _size.clamp(widget.minSize, widget.maxSize);
+            widget.onResize?.call(_size);
+          });
+        },
+        child: Container(
+          width: isHorizontal ? double.infinity : 8,
+          height: isHorizontal ? 8 : double.infinity,
+          decoration: BoxDecoration(
+            color: _isDragging || _isHovering
+                ? Theme.of(context).colorScheme.primary
+                : Theme.of(context).dividerColor,
+            borderRadius: BorderRadius.circular(1),
+          ),
+          child: Center(
+            child: Container(
+              width: isHorizontal ? 48 : 2,
+              height: isHorizontal ? 2 : 48,
+              decoration: BoxDecoration(
+                color: _isDragging || _isHovering
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).dividerColor,
+                borderRadius: BorderRadius.circular(1),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
